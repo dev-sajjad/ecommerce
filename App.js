d2a6d102fc6890666015102b9d0cf155c7c8eb41
Sajjad <sajjad@Sajjads-MacBook-Air.local>
2021-05-19 02:03:09 +0500

    ecommerce

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text,StyleSheet,TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Orderslist from './components/Orderslist';
+import Productslist from './components/Productslist';
+import Employeeslist from './components/employeeslist';
 
 function HomeScreen({ navigation }) {
+  const products=[{id:'100$',name:'shirt',price:'50$',picture:'https://www.affordable.pk/uploads/products/thumbs/large_15556869010_braun_f.jpg',quantity:5,description:'This is Shirt'},
+  {id:2,name:'shoes',price:'100$',picture:'https://5.imimg.com/data5/UW/VK/SQ/ANDROID-64440449/product-jpeg-500x500.jpg',quantity:10,description:'This is shoes'},
+  {id:3,name:'jacket',price:'150$',picture:'https://www.raramart.co.jp/assets/uploads/products/2019-06-05-11-46-25-7.JPG',quantity:15,description:'This is jacket'}];
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Orderslist')}
-      />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start',marginTop:100 }}>
+  
+      <TouchableOpacity
+          style={{backgroundColor: '#808080',padding:20,margin:20}}
+          onPress={() => navigation.navigate('Productslist',products)}
+        >
+          <Text style={{color:'white',fontSize:20,fontWeight:'600'}}>Products List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{backgroundColor: '#87ceeb',padding:20,margin:20}}
+          onPress={() => navigation.navigate('Orderslist')}
+        >
+          <Text style={{color:'white',fontSize:20,fontWeight:'600'}}>Orders List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{backgroundColor: '#654321',padding:20,margin:20}}
+          onPress={() => navigation.navigate('Employeeslist')}
+        >
+          <Text style={{color:'white',fontSize:20,fontWeight:'600'}}>Employees List</Text>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -25,12 +44,18 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Productslist" component={Productslist} />
         <Stack.Screen name="Orderslist" component={Orderslist} />
-        
+        <Stack.Screen name="Employeeslist" component={Employeeslist} />
 
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+
+const styles = StyleSheet.create({
+ 
+ 
+});
 export default App;
--- components/Productslist.js
+++ components/Productslist.js
@@ -1,13 +1,63 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-function Productslist({navigation}) {
+import { Button, View, Text,Image, TouchableOpacity, FlatList } from 'react-native';
+function Productslist({navigation,route}) {
+   
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Products list</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.push('Home')}
+      <View style={{ flex: 1, justifyContent: 'flex-start',margin:50 }}>
+        <Text style={{fontSize:22,fontWeight:'600',marginBottom:20}}>Products list</Text>
+        <View style={{flexDirection:'row',justifyContent:'space-between',borderWidth:2,padding:20}}>
+         <Text style={{fontSize:18}}>Image</Text>
+         <Text style={{fontSize:18}}>Name</Text>
+         <Text style={{fontSize:18}}>Price</Text>
+        </View>
+        <View>
+        <FlatList
+        data={[...route.params]}
+        renderItem={({item},index)=>(
+            <View style={{flexDirection:'row',justifyContent:'space-between',borderWidth:2,padding:20}}>
+            <Image
+              style={{width:50,height:50}}
+              source={{uri:item.picture}}
+            />
+             <Text style={{fontSize:18}}>{item.name}</Text>
+             <Text style={{fontSize:18}}>{item.price}</Text>
+            </View>
+        )}
+        
         />
+
+        </View>
+        
+        <TouchableOpacity
+        onPress={() => navigation.navigate('Home')}
+        >
+        <View style={{flexDirection:'row',justifyContent:'space-between',borderWidth:2,padding:20}}>
+        <Image
+          style={{width:50,height:50}}
+          source={{uri: 'https://www.affordable.pk/uploads/products/thumbs/large_15556869010_braun_f.jpg'}}
+        />
+         <Text style={{fontSize:18}}>Shirt</Text>
+         <Text style={{fontSize:18}}>50$</Text>
+        </View>
+
+        </TouchableOpacity>
+        
+        <View style={{flexDirection:'row',justifyContent:'space-between',borderWidth:2,padding:20}}>
+        <Image
+          style={{width:50,height:50}}
+          source={{uri: 'https://5.imimg.com/data5/UW/VK/SQ/ANDROID-64440449/product-jpeg-500x500.jpg'}}
+        />
+         <Text style={{fontSize:18}}>Shoes</Text>
+         <Text style={{fontSize:18}}>100$</Text>
+        </View>
+        <View style={{flexDirection:'row',justifyContent:'space-between',borderWidth:2,padding:20}}>
+        <Image
+          style={{width:50,height:50}}
+          source={{uri: 'https://www.raramart.co.jp/assets/uploads/products/2019-06-05-11-46-25-7.JPG'}}
+        />
+         <Text style={{fontSize:18}}>Jacket</Text>
+         <Text style={{fontSize:18}}>150$</Text>
+        </View>
       </View>
     );
   }
